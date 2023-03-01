4684ef11ac2c8a5123f109d6282a99420013c69d
mh-lamine <mlamins.ngom@gmail.com>
2023-03-01 17:15:44 +0100

    bds

---
+++ js/main.js
@@ -0,0 +1,6 @@
+parallax = document.getElementById("objectif")
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
