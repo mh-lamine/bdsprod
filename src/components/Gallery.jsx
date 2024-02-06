4f9cda2e56b63e1695f26b7431ce500c983782e5
Mohamed-Lamine Ngom <lamzo@MacBook-Pro-de-Mohamed-Lamine.local>
2024-02-06 08:50:12 +0100

    remove gallery animation

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,26 +1,26 @@
 import { useEffect } from 'react'
 
 export default function Gallery() {
-    useEffect(() => {
-        const images = document.querySelectorAll(".gallery-image");
+    // useEffect(() => {
+    //     const images = document.querySelectorAll(".gallery-image");
 
-        images.forEach((image) => {
-          image.animate(
-            {
-              opacity: [0, 1],
-              clipPath: ["inset(45% 20% 45% 20%)", "inset(0% 0% 0% 0%)"],
-            },
-            {
-              fill: "both",
-              timeline: new ViewTimeline({
-                subject: image,
-              }),
-              rangeStart: "entry 25%",
-              rangeEnd: "cover 50%",
-            }
-          );
-        });
-    }, []);
+    //     images.forEach((image) => {
+    //       image.animate(
+    //         {
+    //           opacity: [0, 1],
+    //           clipPath: ["inset(45% 20% 45% 20%)", "inset(0% 0% 0% 0%)"],
+    //         },
+    //         {
+    //           fill: "both",
+    //           timeline: new ViewTimeline({
+    //             subject: image,
+    //           }),
+    //           rangeStart: "entry 25%",
+    //           rangeEnd: "cover 50%",
+    //         }
+    //       );
+    //     });
+    // }, []);
   return (
     <div className="grid gap-4 py-6 border-b-2">
       <img
